2a08160dab4851f73ea8bea2f238aac359b3ff42
shannonhickman2004 <shannonhickman2004@gmail.com>
2024-10-04 13:05:19 -0600

    week3project

---
+++ scripts/windchill.js
@@ -0,0 +1,23 @@
+
+const wcf = document.querySelector('#windChill');
+
+
+function windChill(temp, speedMs) {
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speedMs, 0.16)) + (0.3965 * temp * Math.pow(speedMs, 0.16));
+}
+
+
+let tempC = 10; 
+let speedKmh = 5; 
+
+
+let speedMs = speedKmh / 3.6; 
+
+
+if (tempC <= 10 && speedKmh > 4.8) {
+    wcf.textContent = windChill(tempC, speedMs).toFixed(1) + "°C";
+} else {
+    wcf.textContent = "N/A";
+}
+
+
